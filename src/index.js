26ee4cb9bdc396a7f4a7d133f8d987802b119ea9
Nadatari <dariosting@gmail.com>
2021-12-08 15:50:41 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+import store from "./store";
+import { bugAdded, bugResolved } from "./actions"
+
+store.dispatch(bugAdded("Bug1"))
+store.dispatch(bugResolved(1))
+
+
+console.log(store)
+console.log(store.getState())
